c4b845879a693465070d9e7ff46c995e4a9a8d60
mbolin <none@none>
2010-08-09 22:33:34 -0400

    First draft of more detailed plovr documentation.
    --HG--
    rename : test/org/plovr/integration-test-config.js => testdata/example/integration-test-config.js
    rename : test/org/plovr/test-raw.html => testdata/example/test-raw.html

--- testdata/example/integration-test-config.js
+++ testdata/example/integration-test-config.js
@@ -1,6 +1,6 @@
 {
   // "This config file is used to test org.plovr.Main.",
   "id": "integration-test",
-  "paths": "testdata",
-  "inputs": "testdata/example/main.js"
+  "paths": ".",
+  "inputs": "main.js"
 }
