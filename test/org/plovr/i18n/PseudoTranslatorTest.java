294f7c07856c4ab33d8649a7a7f39a949c3bd79b
bolinfest@gmail.com <bolinfest@gmail.com>
2010-10-09 15:23:13 -0400

    Edited PseudoTranslatorTest so it works as ASCII.

--- test/org/plovr/i18n/PseudoTranslatorTest.java
+++ test/org/plovr/i18n/PseudoTranslatorTest.java
@@ -46,12 +46,14 @@ public class PseudoTranslatorTest {
     PseudoTranslator.translateXlf(input, writer);
     writer.flush();
 
+    // Tried to maintain this as a UTF-8 file so that this string would be more
+    // readable, but that created far more problems than it solved.
     String translatedXlf = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
         "<xliff version=\"1.2\" xmlns=\"urn:oasis:names:tc:xliff:document:1.2\">\n" +
         "  <file original=\"SoyMsgBundle\" datatype=\"x-soy-msg-bundle\" xml:space=\"preserve\" source-language=\"en\">\n" +
         "    <body>\n" +
         "      <trans-unit id=\"8148892645275058473\" datatype=\"html\">\n" +
-        "        <source>Personal Settings</source><target>Pεrsόηαl Sεttὶηgs</target>\n" +
+        "        <source>Personal Settings</source><target>P\u03B5rs\u1F79\u03B7\u03B1l S\u03B5tt\u1F76\u03B7gs</target>\n" +
         "        <note priority=\"1\" from=\"description\">name/email settings page</note>\n" +
         "        <note priority=\"1\" from=\"meaning\">heading</note>\n" +
         "      </trans-unit>\n" +
