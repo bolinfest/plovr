3d5864e5dbb58f63cdc319c5fa486c69c3c617e0
mbolin <none@none>
2010-11-04 21:02:02 -0400

    fixed modules example to use updated config option names
    --HG--
    extra : rebase_source : c7fe2a7870e2bd8045fca50a45ec7e3330879321

--- testdata/modules/plovr-config.js
+++ testdata/modules/plovr-config.js
@@ -17,9 +17,9 @@
       "deps": ["app"]
     }
   },
-  "module_output_path": "../../build/module-example/module_%s.js",
+  "module-output-path": "../../build/module-example/module_%s.js",
 
   // For a local HTML page, production_uri happens to have the same value as
   // output_path, but for a production system, they would likely be different.
-  "module_production_uri": "../../build/module-example/module_%s.js"
+  "module-production-uri": "../../build/module-example/module_%s.js"
 }
