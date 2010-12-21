bb9c8c6611f1e58290b58b5612a110b197a000d3
Michael Bolin <bolinfest@gmail.com>
2010-12-21 01:00:52 -0500

    Run the modules demo in SIMPLE mode by default and include a healthy comment about why it is problematic in RAW mode. Also fix a CSS issue in demo/modules.soy to prevent unnecessary scrollbars from appearing.

--- testdata/modules/app_init.js
+++ testdata/modules/app_init.js
@@ -28,6 +28,24 @@ moduleManager.setModuleUris(goog.global['PLOVR_MODULE_URIS']);
 // dependencies until it knows it has been loaded.
 moduleManager.setLoaded('app');
 
+// One problem with this use of exports is that it causes problems in RAW mode.
+// These calls to goog.exportSymbol() define an example.api object in the global
+// namespace. Therefore, when api.js is loaded in RAW mode, the call to
+// goog.provide('example.api') throws an error because the pre-existing
+// example.api object results in a "Namespace already declared" error.
+//
+// This is not a problem in SIMPLE or ADVANCED mode because the Compiler
+// sets the value of goog.DEBUG such that goog.provide() no longer checks for
+// duplicate namespaces at runtime because it assumes that the Compiler has
+// already addressed such issues at compile time.
+//
+// The easiest way to solve this problem for RAW mode would be to add the
+// following two exports in their own namespace, though that would likely be
+// awkward for clients of the API. This is effectively what Gmail's Greasemonkey
+// API does (http://code.google.com/p/gmail-greasemonkey/wiki/GmailGreasemonkey10API):
+// the loading code is in the gmonkey.* namespace while the Gmail API is in the
+// gmail.* namespace.
+
 goog.exportSymbol('example.api.load', function(callback) {
   moduleManager.execOnLoad('api', callback);
 });
