0a420c45b0495d2584d90b42cd5bc327c9ccb83a
bolinfest@gmail.com <bolinfest@gmail.com>
2011-01-03 22:20:52 -0500

    Fix integration-test-config.js so that it specifies UTF-8 as the output-charset now that US-ASCII is the default.

--- testdata/example/integration-test-config.js
+++ testdata/example/integration-test-config.js
@@ -2,5 +2,10 @@
   // "This config file is used to test org.plovr.Main."
   "id": "integration-test",
   "paths": ".",
-  "inputs": "main.js"
+  "inputs": "main.js",
+  
+  // This must be specified because datetimesymbols.js from the Closure Library
+  // will be included, so when test-raw.html loads each input in RAW mode,
+  // it is important that the proper charset be used.
+  "output-charset": "UTF-8"
 }
