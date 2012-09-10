7773636bb3e9a8faf8cf0e258fdc0f65c5ee4fbe
Michael Bolin <bolinfest@gmail.com>
2012-09-09 22:59:36 -0700

    Address warnings: add @Override for overridden methods.

--- src/org/plovr/HttpExchangeDelegate.java
+++ src/org/plovr/HttpExchangeDelegate.java
@@ -29,70 +29,87 @@ class HttpExchangeDelegate extends HttpExchange {
     this.exchange = exchange;
   }
 
+  @Override
   public void close() {
     exchange.close();
   }
 
+  @Override
   public boolean equals(Object obj) {
     return exchange.equals(obj);
   }
 
+  @Override
   public Object getAttribute(String arg0) {
     return exchange.getAttribute(arg0);
   }
 
+  @Override
   public HttpContext getHttpContext() {
     return exchange.getHttpContext();
   }
 
+  @Override
   public InetSocketAddress getLocalAddress() {
     return exchange.getLocalAddress();
   }
 
+  @Override
   public HttpPrincipal getPrincipal() {
     return exchange.getPrincipal();
   }
 
+  @Override
   public String getProtocol() {
     return exchange.getProtocol();
   }
 
+  @Override
   public InetSocketAddress getRemoteAddress() {
     return exchange.getRemoteAddress();
   }
 
+  @Override
   public InputStream getRequestBody() {
     return exchange.getRequestBody();
   }
 
+  @Override
   public Headers getRequestHeaders() {
     return exchange.getRequestHeaders();
   }
 
+  @Override
   public String getRequestMethod() {
     return exchange.getRequestMethod();
   }
 
+  @Override
   public URI getRequestURI() {
     return exchange.getRequestURI();
   }
 
+  @Override
   public OutputStream getResponseBody() {
     return exchange.getResponseBody();
   }
 
+  @Override
   public int getResponseCode() {
     return exchange.getResponseCode();
   }
 
+  @Override
   public Headers getResponseHeaders() {
     return exchange.getResponseHeaders();
   }
 
+  @Override
   public int hashCode() {
     return exchange.hashCode();
   }
 
+  @Override
   public void sendResponseHeaders(int arg0, long arg1) throws IOException {
     exchange.sendResponseHeaders(arg0, arg1);
     haveResponseHeadersBeenSent = true;
@@ -102,14 +119,17 @@ class HttpExchangeDelegate extends HttpExchange {
     return haveResponseHeadersBeenSent;
   }
 
+  @Override
   public void setAttribute(String arg0, Object arg1) {
     exchange.setAttribute(arg0, arg1);
   }
 
+  @Override
   public void setStreams(InputStream arg0, OutputStream arg1) {
     exchange.setStreams(arg0, arg1);
   }
 
+  @Override
   public String toString() {
     return exchange.toString();
   }
