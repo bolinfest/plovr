02b85d0291830be4dd63ddaa811df1079e6b40cd
Michael Bolin <bolinfest@gmail.com>
2013-01-06 16:05:08 -0800

    Upgrade GSON to version 2.2.2.
    Also created a unit test to verify that setLenient() has some terrible behavior.
    Filed http://code.google.com/p/google-gson/issues/detail?id=494 to get it fixed.

--- test/org/plovr/util/GsonUtilTest.java
+++ test/org/plovr/util/GsonUtilTest.java
@@ -1,15 +1,21 @@
 package org.plovr.util;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.io.StringReader;
 
 import org.junit.Test;
 import org.plovr.GsonUtil;
 
 import com.google.common.collect.ImmutableList;
 import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonNull;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.google.gson.JsonPrimitive;
+import com.google.gson.stream.JsonReader;
 
 /**
  * {@link GsonUtilTest} is a unit test for {@link GsonUtil}.
@@ -62,5 +68,31 @@ public class GsonUtilTest {
     array.add(new JsonPrimitive("foo"));
     array.add(new JsonPrimitive(400));
     GsonUtil.toListOfStrings(array);
+  }
+
+  /**
+   * Tests what behavior is allowed by {@link JsonReader#setLenient(boolean)}.
+   */
+  @Test
+  public void testLenientJsonReader() {
+    StringReader reader = new StringReader("{foo: ['bar', 'baz',]}");
+    JsonReader jsonReader = new JsonReader(reader);
+    jsonReader.setLenient(true);
+    JsonParser parser = new JsonParser();
+    JsonElement root = parser.parse(jsonReader);
+
+    assertTrue(root.isJsonObject());
+    JsonObject jsonObject = root.getAsJsonObject();
+
+    assertTrue(jsonObject.has("foo"));
+    JsonElement fooValue = jsonObject.get("foo");
+    assertTrue(fooValue.isJsonArray());
+
+    // This is atrocious: this should parse as a two element array.
+    JsonArray fooArray = fooValue.getAsJsonArray();
+    assertEquals(3, fooArray.size());
+    assertEquals("bar", fooArray.get(0).getAsString());
+    assertEquals("baz", fooArray.get(1).getAsString());
+    assertTrue(fooArray.get(2).isJsonNull());
   }
 }
