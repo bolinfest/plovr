6f24b03cffc27a2c9d2bf7469cd44fae967315e3
Rick Power <rickp@celtx.com>
2017-04-11 12:47:40 -0300

    Fixes for recent closure-templates updates
    Accomodate pom.xml dependency requirements for latest
    closure-templates updates:
    
    1) auto-value@1.3
    2) javacc@6.1.2
    
    Update gen_parser.sh to reflect that TypeParser.jj no longer
    exists in closure-templates
    
    Lastly, fix testInputNames failure in ConfigParserTest

--- closure/closure-templates/gen_parser.sh
+++ closure/closure-templates/gen_parser.sh
@@ -5,9 +5,8 @@
 JAVACC="$1"
 EXPRESSION_PARSER_JJ="$2"
 SOY_FILE_PARSER_JJ="$3"
-TYPE_PARSER_JJ="$4"
-OUTPUT_FILE="$5"
-TMP="$6"
+OUTPUT_FILE="$4"
+TMP="$5"
 
 cd $TMP
 
@@ -18,9 +17,5 @@ mv *.java com/google/template/soy/exprparse
 mkdir -p com/google/template/soy/soyparse
 java -classpath $JAVACC org.javacc.parser.Main $SOY_FILE_PARSER_JJ &> /dev/null
 mv *.java com/google/template/soy/soyparse
-
-mkdir -p com/google/template/soy/types/parse
-java -classpath $JAVACC org.javacc.parser.Main $TYPE_PARSER_JJ &> /dev/null
-mv *.java com/google/template/soy/types/parse
 
 zip -r $OUTPUT_FILE *
